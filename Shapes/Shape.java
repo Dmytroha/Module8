83d4390d481e6ab5680dd2b4aa70fbef501d6d6e
Dmytro Milovanov <milovanovu@gmail.com>
2023-04-05 05:44:42 +0200

    first commit to share code

---
+++ Shapes/Shape.java
@@ -0,0 +1,6 @@
+package module8;
+public abstract class Shape {
+    // add name to abstract class to avoid adding it in every child class
+    private String name;
+    abstract String getName();
+}
